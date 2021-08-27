2b8b6ccaf4ce108eea1580bd648105ce32f82d76
yuki <wkiki1124@gmail.com>
2021-08-28 02:26:40 +0800

    commit

--- shoppingAPP/ViewController.swift
+++ shoppingAPP/ViewController.swift
@@ -14,8 +14,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet var imagesSwipeGestureRecognizer: UISwipeGestureRecognizer!
     @IBOutlet var imagesRightSwipeGestureRecognizer: UISwipeGestureRecognizer!
-    
-    
     @IBOutlet weak var informationSegmentedControl: UISegmentedControl!
     @IBOutlet weak var pageLabel: UILabel!
     @IBOutlet weak var piecesLabel: UILabel!
@@ -26,9 +24,7 @@ class ViewController: UIViewController {
     
     
     let dressImageNameArray = ["dress0","dress1","dress2","dress3","dress4","dress5","dress6","dress7","dress8"]
-    let coatImageNameArray = ["coat0","coat1","coat2","coat3","coat4","coat5","coat6","coat7","coat8"]
-    let kidImageNameArray = ["kid0","kid1","kid2","kid3","kid4","kid5","kid6","kid7"]
-    
+
     var indexNumber: Int = 0
     var pageNumber: Int = 1
     var pieces: Int = 0
@@ -57,11 +53,9 @@ class ViewController: UIViewController {
 
     func changeSync() {
         dressImageView.image = UIImage(named: dressImageNameArray[indexNumber])
-        pageControl.currentPage = indexNumber
         pageNumber = indexNumber + 1
+        pageControl.currentPage = indexNumber
         pageLabel.text = " \(pageNumber) / 9 "
-        
-        
     }
     
     
@@ -86,39 +80,30 @@ class ViewController: UIViewController {
     }
     
     @IBAction func changeContents(_ sender: UIPageControl) {
-      
         if pageControl.currentPage == 0 {
             indexNumber = 0
             changeSync()
-            
         } else if pageControl.currentPage == 1 {
             indexNumber = 1
             changeSync()
-            
         } else if pageControl.currentPage == 2 {
             indexNumber = 2
             changeSync()
-            
         } else if pageControl.currentPage == 3 {
             indexNumber = 3
             changeSync()
-            
         } else if pageControl.currentPage == 4 {
             indexNumber = 4
             changeSync()
-            
         } else if pageControl.currentPage == 5 {
             indexNumber = 5
             changeSync()
-            
         } else if pageControl.currentPage == 6 {
             indexNumber = 6
             changeSync()
-            
         } else if pageControl.currentPage == 7 {
             indexNumber = 7
             changeSync()
-            
         } else if pageControl.currentPage == 8 {
             indexNumber = 8
             changeSync()
@@ -172,8 +157,6 @@ class ViewController: UIViewController {
                 }
         }
     }
-    
-
     
 
 
